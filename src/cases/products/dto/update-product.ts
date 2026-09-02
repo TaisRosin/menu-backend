026383b627311c5194c10280b1cc8e6491b8d4f6
Taís Rosin <taisrosin.1508@gmail.com>
2026-09-01 21:59:14 -0300

    Aula dia 01/09

---
+++ src/cases/products/dto/update-product.ts
@@ -0,0 +1,39 @@
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUrl,
+  IsUUID,
+  MaxLength,
+  Min,
+} from 'class-validator';
+
+export class UpdateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(60)
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @Min(0)
+  price: number;
+
+  @IsOptional()
+  @IsUUID()
+  categoryId?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  active?: boolean;
+
+  @IsOptional()
+  @IsUrl({ require_protocol: true })
+  picture?: string;
+}
