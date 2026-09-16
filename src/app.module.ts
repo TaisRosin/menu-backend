840c8f1912e2cd98ca7d0339fb9a66e5d7653d31
Taís Rosin <taisrosin.1508@gmail.com>
2026-09-15 21:56:37 -0300

    Aula 15/09

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { CategoryModule } from './cases/categories/category.module';
 import { ProductModule } from './cases/products/product.module';
 import { SpotModule } from './cases/spots/spot.module';
 import { GuestCheckModule } from './cases/guest-checks/guest-check.module';
+import { OrderModule } from './cases/orders/order.module';
 
 @Module({
   imports: [
@@ -31,7 +32,8 @@ import { GuestCheckModule } from './cases/guest-checks/guest-check.module';
     CategoryModule,
     ProductModule,
     SpotModule,
-    GuestCheckModule
+    GuestCheckModule,
+    OrderModule
   ],
   controllers: [],
   providers: [],
